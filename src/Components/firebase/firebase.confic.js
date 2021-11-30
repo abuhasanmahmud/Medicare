b58c49bfb39694a79f8686e599648c725b6f6f5c
mahmuddeveloper <mahmud.me420@gmail.com>
2021-11-30 16:05:43 +0600

    done

---
+++ src/Components/firebase/firebase.confic.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA99TPdtpU_7ndzg963S2kch9U9y7WV-b0",
+  authDomain: "medicare-8c960.firebaseapp.com",
+  projectId: "medicare-8c960",
+  storageBucket: "medicare-8c960.appspot.com",
+  messagingSenderId: "497572488503",
+  appId: "1:497572488503:web:9e4ce5b368794be6baa6b2"
+};
+export default firebaseConfig;
